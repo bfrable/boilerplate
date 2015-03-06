3e2891e8116f8c65f3be561b2428eb0cd602d703
Brett Frable <bfrable@gmail.com>
2015-03-05 20:42:50 -0500

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    serve: {
+        options: {
+            port: 9000
+        }
+    },
+    sass: {
+        options: {
+            quiet: true
+        },
+        dist: {
+          files: [{
+            expand: true,
+            cwd: 'sass',
+            src: ['**/*.scss'],
+            dest: 'assets/styles',
+            ext: '.css'
+          }]
+        }
+    },
+    concat: {
+      js: {
+        src: ['assets/scripts/*.js'],
+        dest: 'assets/build/scripts/build.js',
+        options: {
+          separator: ';'
+        }
+      },
+    },
+    uglify: {
+      js: {
+        src: 'assets/build/scripts/build.js',
+        dest: 'assets/build/scripts/build.min.js'
+      }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'assets/styles',
+          src: ['*.css'],
+          dest: 'assets/build/styles',
+          ext: '.min.css'
+        }]
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+      },
+      js: {
+        files: ['assets/scripts/*.js'],
+        tasks: ['concat', 'uglify']
+      },
+      css: {
+        files: ['sass/**/*.scss', 'assets/styles/*.css', 'assets/build/styles/main.min.scss'],
+        tasks: ['sass', 'cssmin']
+      },
+      html: {
+        files: ['index.php', 'includes/*.php']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-serve');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', ['sass', 'concat', 'uglify', 'cssmin']);
+};
---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+    <title>Boilerplate</title>
+    
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/build/styles/main.min.css">
+
+  </head>
+  
+  <body>
+    
+    <!--[if lt IE 10]>
+    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    
+    <?php include 'includes/header.php'; ?>
+
+    <div id="main">
+
+      <div class="wrapper">
+        Content Here
+      </div>
+   
+    </div>
+
+    <?php include 'includes/footer.php'; ?>
+
+    <!-- vendors -->
+    <script src="vendors/jquery/dist/jquery.js"></script>
+    <!-- end vendors -->
+    
+    <!-- scripts -->
+    <script src="assets/build/scripts/build.js"></script>
+    <!-- end scripts -->
+
+  </body>
+</html>
